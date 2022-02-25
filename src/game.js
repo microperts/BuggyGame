eed2ab82ed70d955ef2778ea28891150eb3d51f0
Mahnoor Babar <47009449+Mahnoor-B@users.noreply.github.com>
2022-02-25 17:53:36 +0500

    set sensor

--- src/game.js
+++ src/game.js
@@ -3,6 +3,8 @@ import Phaser from 'phaser'
 let clock, time, totalTime = 0, lapTime
 let bg, buggy
 
+let sensorBody, playerBody, playerSprite, sensorSprite
+
 let vec, dx,dy, vx,vy, speed = 2
 
 export default class Game extends Phaser.Scene
@@ -19,7 +21,44 @@ export default class Game extends Phaser.Scene
 
         bg = this.add.image(400, 225, 'bg').setScale(0.5)
         this.add.image(-800, 225, 'bg').setScale(0.5).setFlipX(true)
-        buggy = this.matter.add.sprite(400, 250, 'buggy').setScale(0.3)
+        buggy = this.matter.add.sprite(400, 250, 'buggy', undefined, {label: 'buggy'}).setScale(0.3)
+        this.buggy2 = this.matter.add.sprite(200, 150, 'buggy', undefined,
+        {
+            label: 'grass'
+        }).setScale(0.3)
+        this.buggy2.setSensor(true)
+
+        // buggy.body.collisionFilter.group = 1
+        // buggy.body.collisionFilter.mask = 0
+
+        this.matter.world.on('collisionstart', (event)=>{
+            let pair = event.pairs
+            for (var i = 0; i < pair.length; i++)
+            {
+                this.bodyA = pair[i].bodyA;
+                this.bodyB = pair[i].bodyB;
+
+                console.log(this.bodyA)
+
+                if(this.bodyA.isSensor === true){
+                    sensorBody = this.bodyB
+                    playerBody = this.bodyA
+                }
+
+                else if(this.bodyB.isSensor === true){
+                    sensorBody = this.bodyA
+                    playerBody = this.bodyB
+                }
+
+                playerSprite = playerBody.gameObject
+                sensorSprite = sensorBody.gameObject
+            
+                console.log("Sensor body: "+sensorBody.label)
+                if(playerBody.label === 'grass'){
+                    sensorSprite.setFriction(10)
+                }
+            }
+        })
 
         lapTime = this.add.text(560, 500, '').setScrollFactor(0)
     }
@@ -44,7 +83,7 @@ export default class Game extends Phaser.Scene
             
             speed +=0.002
 
-            console.log('speed: '+speed)
+            // console.log('speed: '+speed)
             vx = Math.cos(vec) * speed
             vy = Math.sin(vec) * speed
 
